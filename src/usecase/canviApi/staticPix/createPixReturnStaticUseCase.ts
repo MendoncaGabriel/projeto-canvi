1988db3852343a2db419f8c71d2f3eb47c3b607c
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-04-26 14:24:12 -0400

    feat: create pix return static usecase

---
+++ src/usecase/canviApi/staticPix/createPixReturnStaticUseCase.ts
@@ -0,0 +1,19 @@
+import {
+  CreatePixReturnStaticRequest,
+  CreatePixReturnStaticResponse,
+} from "@/types/canvi";
+import { AxiosInstance } from "axios";
+
+export class createPixReturnStaticUseCase {
+  constructor(private readonly axiosClient: AxiosInstance) {}
+
+  async execute(
+    body: CreatePixReturnStaticRequest
+  ): Promise<CreatePixReturnStaticResponse> {
+    const { data } = await this.axiosClient.post(
+      "/pix/estatico/devolucao",
+      body
+    );
+    return data;
+  }
+}
