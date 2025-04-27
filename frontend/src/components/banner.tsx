3c5421f0201d0abb2a0335c2e0a61517446cc1fa
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-04-27 18:05:41 -0400

    fix: responsivity text banner principal

--- frontend/src/components/banner.tsx
+++ frontend/src/components/banner.tsx
@@ -17,15 +17,15 @@ export function Banner() {
         className="w-full h-full object-cover absolute"
         style={{ height: 'calc(100vh - 200px)' }}
       />
-      <div className="absolute p-4 md:p-8 lg:p-10 max-w-[90%] md:max-w-[80%] lg:max-w-[70%]">
-        <p className="text-3xl md:text-4xl lg:text-5xl font-bold text-orange-200 mb-4">
+      <div className="absolute p-4 md:p-8 lg:p-10 ">
+        <p className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-orange-200 mb-4 leading-tight">
           Transforme Vidas com Amor!
         </p>
-        <p className="text-xl md:text-2xl lg:text-3xl font-bold text-orange-400 mb-4">
+        <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-orange-400 mb-4 leading-tight">
           Doe quanto quiser ou compre produtos <br className="hidden md:block" />
           e ajude animais que precisam de um lar.
         </p>
-        <p className="text-2xl md:text-3xl lg:text-4xl font-bold text-orange-200">
+        <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-orange-200 leading-tight">
           Cada compra é um gesto de amor!
         </p>
       </div>
