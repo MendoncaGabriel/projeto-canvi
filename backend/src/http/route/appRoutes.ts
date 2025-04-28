1c104ce5376c7c2a41dff1b98fe6802ba2310830
Gabriel Andrade <138128244+MendoncaGabriel@users.noreply.github.com>
2025-04-28 12:55:00 -0400

    fix: remove import becuse it is not being used

--- backend/src/http/route/appRoutes.ts
+++ backend/src/http/route/appRoutes.ts
@@ -1,8 +1,5 @@
 import { FastifyInstance } from "fastify";
 import { Auth } from "../middleware/auth";
-import ejs from "ejs";
-import fs from "fs";
-import path from "path";
 
 export function AppRoutes(app: FastifyInstance) {
   app.get("/auth", { preHandler: [Auth] }, () => ({
